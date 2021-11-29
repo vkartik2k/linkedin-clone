bef18b7684b49210d6546e7afccc1cb4ba6816aa
vkartik2k <vkartik2k@gmail.com>
2021-11-29 21:32:37 +0530

    code

---
+++ src/components/Fixed.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Fixed() {
+    return (
+        <div style={{height:350, width:250, backgroundColor: 'white', borderRadius: 10, margin: 10, position: 'sticky', top: 90}}>
+            Fixed component
+        </div>
+    )
+}
+
+export default Fixed
---
+++ src/components/Profile.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Profile() {
+    return (
+        <div style={{height:350, width:250, backgroundColor: 'white', borderRadius: 10, margin: 10}}>
+            Profile component
+        </div>
+    )
+}
+
+export default Profile
